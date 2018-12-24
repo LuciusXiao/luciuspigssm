7be2b9305f871aee2472e30bb50fccc34f741a9c
lucius <879133505@qq.com>
2018-12-24 08:27:03 +0800

    Initial commit

---
+++ src/main/java/com/luciuspig/model/user/Account.java
@@ -0,0 +1,57 @@
+package com.luciuspig.model.user;
+
+import java.io.Serializable;
+
+/**
+ * 账号
+ * @author Lucius
+ * */
+public class Account implements Serializable {
+
+    private Long id;
+
+    /**账号*/
+    private String account;
+
+    /**密码 (md5：密码+盐)*/
+    private  String password;
+
+    /**是否启用*/
+    private Boolean enable;
+
+    public Account() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+}
---
+++ src/main/java/com/luciuspig/model/user/User.java
@@ -0,0 +1,23 @@
+package com.luciuspig.model.user;
+
+import java.io.Serializable;
+
+/**
+ * 用户
+ * @author lucius
+ * */
+public class User implements Serializable {
+
+    /**id*/
+    private Long id;
+
+    /**账号*/
+    private Account account;
+
+    /**姓名*/
+    private String name;
+
+    /***/
+
+
+}
