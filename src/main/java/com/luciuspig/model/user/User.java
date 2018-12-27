a60db2f5fb96039abfcb171c0665d956c6beb30f
lucius <87913305@qq.com>
2018-12-27 11:30:44 +0800

    完善User表

--- src/main/java/com/luciuspig/model/user/User.java
+++ src/main/java/com/luciuspig/model/user/User.java
@@ -17,7 +17,44 @@ public class User implements Serializable {
     /**姓名*/
     private String name;
 
-    /***/
+    /** 手机号*/
+    private  String mobilePhone;
 
+    public User() {
+    }
+    public User(Long id) {
+        this.id = id;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
 }
